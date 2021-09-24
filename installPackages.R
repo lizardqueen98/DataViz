5132aadea5a651a65664a67df37e794c1f635a8b
lizardqueen98 <44054497+lizardqueen98@users.noreply.github.com>
2021-09-24 15:54:41 +0200

    Add files via upload

---
+++ installPackages.R
@@ -0,0 +1,23 @@
+install.packages("colorspace")
+install.packages("RColorBrewer")
+
+install.packages('magrittr') # pipe operator
+install.packages('dplyr') # select, filter, etc.. operations on dataframes
+install.packages('tidyr') # helps making our data tidy with gather, spread and other functions
+install.packages('ggplot2') # plotting library, grammar of graphics
+install.packages('lubridate') # enables easy working with dates
+install.packages('prettydoc') # makes r notebooks prettier
+install.packages('corrplot') # library for correlation plots
+install.packages('tidyverse') # includes multiple packages for cleaning and working with tidy data
+
+install.packages('dslabs') # includes multiple interesting datasets
+install.packages('ggthemes') # makes ggplots nicer with nice themes
+install.packages('ggrepel') # nice geom_text placing on ggplots
+install.packages('gridExtra') # arrange multiple ggplots in one plot (grid of plots)
+
+install.packages('devtools')
+install.packages('plotly')
+install.packages('gapminder')
+
+install.packages("webshot")
+webshot::install_phantomjs()
